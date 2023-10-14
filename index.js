efbe3e33bd515e654bd67dc74a86b3585e11f065
claudioares <claudiojasoares.dev@gmail.com>
2023-10-14 01:08:38 -0300

    Aplicação Coverter 1.0.1 finalizado

--- index.js
+++ index.js
@@ -4,42 +4,49 @@ convertButton.addEventListener('click', () => {
     
     const fileInput = document.querySelector('#fileInput');
     const downloadLink = document.querySelector('#downloadLink');
-
+    if(!fileInput.value) return alert("Clique no botão 'Procurar um documento'");
+    
     const file = fileInput.files[0];
     const formatFile = file.type;
 
-    if (formatFile === "image/jpeg" || formatFile === "image/png") {
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      const img = new Image();
+    try {
 
-      img.onload = function () {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        ctx.drawImage(img, 0, 0, img.width, img.height);
+      if (formatFile === "image/jpeg" || formatFile === "image/png") {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const img = new Image();
 
-        // Converter para PDF usando pdfmake
-        const docDefinition = {
-          content: [
-            { image: canvas.toDataURL('image/png'), width: 500 },
-          ],
+        img.onload = function () {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0, img.width, img.height);
+
+          // Converter para PDF usando pdfmake
+          const docDefinition = {
+            content: [
+              { image: canvas.toDataURL('image/png'), width: 500 },
+            ],
+          };
+
+          pdfMake.createPdf(docDefinition).getBlob((blob) => {
+            const url = URL.createObjectURL(blob);
+            downloadLink.href = url;
+            downloadLink.style.display = 'flex';
+
+            downloadLink.addEventListener('click', () =>{
+              fileInput.value = "";
+              downloadLink.style.display = 'none';
+            })
+          });
         };
 
-        pdfMake.createPdf(docDefinition).getBlob((blob) => {
-          const url = URL.createObjectURL(blob);
-          downloadLink.href = url;
-          downloadLink.style.display = 'flex';
-
-          downloadLink.addEventListener('click', () =>{
-            fileInput.value = "";
-            downloadLink.style.display = 'none';
-          })
-        });
-      };
-
-      img.src = URL.createObjectURL(file);
-    } else {
-        alert("Escolha um arquivo .jpg ou .png");
-        fileInput.value = "";
+        img.src = URL.createObjectURL(file);
+      } else {
+          alert("Escolha um arquivo .jpg ou .png");
+          fileInput.value = "";
+      }
+      
+    } catch (error) {
+      console.log(error)
     }
   });
